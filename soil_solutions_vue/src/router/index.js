37044b5cbe2aa8fca49fabf7de3bff27fb1f75cd
Vedant-Mhatre <vedantmh@gmail.com>
2023-06-23 01:09:06 +0530

    added product page template

--- soil_solutions_vue/src/router/index.js
+++ soil_solutions_vue/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Product from '../views/ProductView.vue'
+
 const routes = [
   {
     path: '/',
@@ -13,7 +15,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product
   }
 ]
 
--- soil_solutions_vue/src/views/HomeView.vue
+++ soil_solutions_vue/src/views/HomeView.vue
@@ -27,6 +27,10 @@
 
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">
+            View details
+          </router-link>
         </div>
 
       </div>
@@ -68,8 +72,11 @@ export default {
 
 <style scoped>
 .image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
+  width: 10rem;
+  height: 10rem;
+}
+
+.is-size-4 {
+  margin-top: 6rem;
 }
 </style>
