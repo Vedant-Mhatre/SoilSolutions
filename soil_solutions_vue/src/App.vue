3667c104e48fb4e4e7f00af3d51b3a3c549be47b
Vedant-Mhatre <vedantmh@gmail.com>
2023-06-25 12:05:13 +0530

    UI improvements

--- soil_solutions_vue/src/App.vue
+++ soil_solutions_vue/src/App.vue
@@ -2,30 +2,26 @@
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>Soil Solutions</strong></router-link>
-
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
+        <router-link to="/" class="navbar-item">Soil Solutions</router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu">
+          <span></span>
+          <span></span>
+          <span></span>
         </a>
       </div>
-      
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
 
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
         <div class="navbar-start">
           <div class="navbar-item">
-            <form method="get" action="/search">
+            <form method="get" action="/search" class="search-form">
               <div class="field has-addons">
                 <div class="control">
-                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                  <input type="text" class="input" placeholder="Search products" name="query">
                 </div>
-
                 <div class="control">
                   <button class="button is-success">
-                      <span class="icon">
-                      <i class="fas fa-search"></i>
-                      </span>
+                    <i class="fas fa-search"></i>
                   </button>
                 </div>
               </div>
@@ -34,29 +30,23 @@
         </div>
 
         <div class="navbar-end">
-            <router-link to="/fertilizers" class="navbar-item">Fertilizers</router-link>
-            <router-link to="/seeds" class="navbar-item">Seeds</router-link>
-
-            <div class="navbar-item">
-              <div class="buttons">
-
-                <router-link to="/log-in" class="button is-light">Log In</router-link>
-                <router-link to="/cart" class="button is-success">
-                  <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                  <span>Cart {{ cartTotalLength }} </span>
-                </router-link>
-              </div>
-            </div>
+          <router-link to="/fertilizers" class="navbar-item">Fertilizers</router-link>
+          <router-link to="/seeds" class="navbar-item">Seeds</router-link>
+          <router-link to="/log-in" class="navbar-item">Log In</router-link>
+          <router-link to="/cart" class="navbar-item">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Cart {{ cartTotalLength }}</span>
+          </router-link>
         </div>
       </div>
     </nav>
 
     <section class="section">
-      <router-view/>
+      <router-view />
     </section>
 
     <footer class="footer">
-      <p class="has-text-centered">Copyright (c) 2023</p>
+      <p class="has-text-centered">© 2023 Soil Solutions</p>
     </footer>
   </div>
 </template>
@@ -78,16 +68,15 @@ export default {
     this.cart = this.$store.state.cart // Set the component's 'cart' data property to the value from the store
   },
   computed: {
-    cartTotalLength(){
-
+    cartTotalLength() {
       let totalLength = 0
-      
+
       // Calculate the total length or quantity of items in the cart
-      for (let i=0; i < this.cart.items.length; i++){
+      for (let i = 0; i < this.cart.items.length; i++) {
         totalLength += this.cart.items[i].quantity
       }
 
-      return totalLength;
+      return totalLength
     }
   }
 }
@@ -95,13 +84,47 @@ export default {
 
 <style lang="scss">
 @import '../node_modules/bulma';
+
+/* Navbar */
+.navbar-brand {
+  padding: 0.75rem 1.5rem;
+}
+
+.navbar-item {
+  align-items: center;
+}
+
 .navbar-item .input {
-  width: 800px; /* Adjust the width as desired */
+  width: 800px;
 }
 
 .navbar-start {
   flex-grow: 1;
   display: flex;
   justify-content: center;
+}
+
+.navbar-burger {
+  color: #fff;
+}
+
+/* Search Bar */
+.search-form {
+  max-width: 800px;
+  width: 100%;
+}
+
+.search-form .control {
+  width: 100%;
+}
+
+/* Section and Footer */
+.section {
+  padding: 3rem 1.5rem;
+}
+
+.footer {
+  padding: 2rem 1.5rem;
+  background-color: #f5f5f5;
 }
 </style>
